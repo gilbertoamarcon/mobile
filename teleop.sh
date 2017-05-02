11a49ca3343b11fe58ca152ca5bd40dce8194465
Gilberto Marcon <gilbertoamarcon@gmail.com>
2017-05-01 20:11:42 -0700

    New vel scale

--- teleop.sh
+++ teleop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 vel=0
 key=w
+vfwd=(0 5 10 15 20 40 60 80 90 100)
+vbwd=(0 5 10 15 20 40 60 80 90 100)
 while true; do
 	read -n 1 resp
 	case $resp in
@@ -29,31 +31,31 @@ while true; do
 			key=x
 			;;
 		9)
-			vel=100
+			vel=9
 			;;
 		8)
-			vel=90
+			vel=8
 			;;
 		7)
-			vel=80
+			vel=7
 			;;
 		6)
-			vel=60
+			vel=6
 			;;
 		5)
-			vel=40
+			vel=5
 			;;
 		4)
-			vel=20
+			vel=4
 			;;
 		3)
-			vel=15
+			vel=3
 			;;
 		2)
-			vel=10
+			vel=2
 			;;
 		1)
-			vel=5
+			vel=1
 			;;
 		0)
 			vel=0
@@ -61,28 +63,28 @@ while true; do
 	esac
 	case $key in
 		w)
-			printf " $(($vel*2))\n$(($vel*2))\n0" > vel
+			printf " ${vfwd[vel]}\n${vfwd[vel]}\n0" > vel
 			;;
 		s)
-			printf " -$vel\n-$vel\n0" > vel
+			printf " -${vbwd[vel]}\n-${vbwd[vel]}\n0" > vel
 			;;
 		a)
-			printf " -$(($vel/2))\n$(($vel*2))\n0" > vel
+			printf " -${vbwd[vel]}\n${vfwd[vel]}\n0" > vel
 			;;
 		d)
-			printf " $(($vel*2))\n-$(($vel/2))\n0)" > vel
+			printf " ${vfwd[vel]}\n-${vbwd[vel]}\n0)" > vel
 			;;
-		q)
-			printf " $(($vel*2))\n$(($vel*3))\n0" > vel
-			;;
-		e)
-			printf " $(($vel*3))\n$(($vel*2))\n0" > vel
-			;;
+		# q)
+		# 	printf " $(($vel*2))\n$(($vel*3))\n0" > vel
+		# 	;;
+		# e)
+		# 	printf " $(($vel*3))\n$(($vel*2))\n0" > vel
+		# 	;;
 		z)
-			printf " 0\n0\n $vel" > vel
+			printf " 0\n0\n ${vfwd[vel]}" > vel
 			;;
 		x)
-			printf " 0\n0\n-$vel" > vel
+			printf " 0\n0\n-${vfwd[vel]}" > vel
 			;;
 	esac
 done
